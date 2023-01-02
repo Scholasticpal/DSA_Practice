35c74b7e64220245d1db2bed13ab136e081c345e
Anmol Garg <80644106+Scholasticpal@users.noreply.github.com>
2023-01-03 01:08:41 +0530

    Create CombinationSumIII.cpp

---
+++ Microsoft Questions/CombinationSumIII.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void comb(int ind, int sum, int k, int n, vector<int>& temp, vector<vector<int>>& ans) {
+
+        if(sum == n && k==0){
+                ans.push_back(temp);
+                return;
+            
+        }
+        if(sum>n){
+            return;
+        }
+        for(int i = ind; i <= 9; i++){
+            if(i>n){
+                break;
+            }
+            temp.push_back(i);
+            comb(i+1, sum+i, k-1, n, temp, ans);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum3(int k, int n){
+     vector<int> temp;
+    vector<vector<int>> ans;
+    comb(1, 0, k, n, temp, ans);
+    return ans;
+    }
+};
