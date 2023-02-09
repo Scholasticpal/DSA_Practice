fe3f39a49f74563ef126c3232dc445e3e707ac92
Anmol Garg <anmolgarg240@gmail.com>
2023-02-09 11:20:41 +0530

    i

---
+++ GFG/LinkedList/LL_Insert_at_pos.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* insertAtPos(Node *head, int pos, int data){
+    Node *temp = new Node(data);
+    
+    if(pos == 1){
+        temp->next = head;
+        return temp;
+    }
+
+    Node *curr = head;
+    for(int i = 1; i<= pos-2 && curr != NULL; i++){
+        curr = curr-> next;
+    }
+    if(curr == NULL) return head;
+
+    temp ->next = curr->next;
+    curr->next = temp;
+    return head;
+}
+
+int main(){
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    int pos = 2;
+    int data = 45;
+    insertAtPos(head, pos, data);
+    return 0;
+}
