6d0b55d2e39490c62d2374930a683461a155545b
Anmol Garg <anmolgarg240@gmail.com>
2023-02-20 03:35:04 +0530

    in

---
+++ GFG/Trees/Intro.cpp
@@ -0,0 +1,15 @@
+/*Tree Data Structure
+    
+    node: the values in a tree
+    root: the topmost node
+    leaf: the bottom-most nodes
+    chil: the adjacent nodes below root
+    parent: the adjacent node on top of the children nodes
+    subtree: the tree within the tree
+    descendants: all the nodes below any given node in the tree
+    ancestors: the nodes above given node, directly connected to given node
+    degree: the no. of children nodes of a node
+    internal nodes: nodes that are not leaf nodes
+ 
+ Note: all leaf nodes have degree 0
+ */
