9b2d0d5d0492597434289b257e3cecfab7feaa42
Anmol Garg <anmolgarg240@gmail.com>
2023-02-22 16:17:14 +0530

    in

---
+++ GFG/Trees/PostorderTraversal.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int key;
+    Node *left;
+    Node *right;
+    Node (int k){
+        key = k;
+        left = right = NULL;
+    }
+};
+void postorder(Node *root){
+    if(root == NULL){return;}
+    postorder(root->left);
+    postorder(root->right);
+    cout<<(root->key)<<" ";
+}
+
+int main(){
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+
+    postorder(root);
+    return 0;
+}
---
+++ GFG/Trees/PreorderTraversal.cpp
@@ -0,0 +1,32 @@
+// TC: O(n) - n is no. of nodes
+// SC: O(H) - H is height of binary tree
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k){
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void preorder(Node *root){
+    if(root == NULL){return;}
+    cout<<(root->key)<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+
+    preorder(root);
+    return 0;
+}
